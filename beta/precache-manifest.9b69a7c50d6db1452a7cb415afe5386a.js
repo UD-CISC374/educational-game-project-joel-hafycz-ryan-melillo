29cb61fb25c339b7ba33583b16da05e6c934fccc
jhaf <jhaf@udel.edu>
2020-05-05 10:32:08 -0400

    Updates

--- beta/precache-manifest.9b69a7c50d6db1452a7cb415afe5386a.js
+++ beta/precache-manifest.9b69a7c50d6db1452a7cb415afe5386a.js
@@ -44,23 +44,47 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/box3.png"
   },
   {
+    "revision": "20cdc48404b16157d207a866690b4108",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/box4.png"
+  },
+  {
+    "revision": "85c8723b4ab18b965f4c4a4e44c0ae32",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/box5.png"
+  },
+  {
+    "revision": "749f45e118968f82c649392008df18b8",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/box6.png"
+  },
+  {
     "revision": "8109a18bcd4c855e62f0069046af4e5a",
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/coin.png"
   },
   {
-    "revision": "2b081544cf09295c904737906151a03a",
+    "revision": "8825f5cc76705b7f6a12b2fc178bbd32",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/door.png"
+  },
+  {
+    "revision": "3b7ec53d9213677d1af75ca4f9aadeaf",
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/emptybox.png"
+  },
+  {
+    "revision": "b760c14cb106df6d9bc739bf2eee2342",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/emptyslot.png"
   },
   {
     "revision": "953a3a96056e5e558905737fe64b6e10",
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/enemy.png"
   },
   {
-    "revision": "32305e30fad719b6a909cec941752f3a",
+    "revision": "7ee054749069a2e9ac760d994fe0dc71",
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/in progress.png"
   },
   {
-    "revision": "ac09d8f14330eb454bfd5edf81a8d133",
+    "revision": "3e6619b72debf623d833bf7e9a4e0378",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/levelchanger.png"
+  },
+  {
+    "revision": "985fef120e95950599e1a29e985859e5",
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/machine_increase.png"
   },
   {
@@ -70,6 +94,30 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e3739efb231a5f4a184d25f294fbec6c",
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/robotstanding.png"
+  },
+  {
+    "revision": "08b78eb0577837851a8f7bd7530eea9a",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/slot1.png"
+  },
+  {
+    "revision": "ff9dcee4b58319e4025545f8e3cb540f",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/slot2.png"
+  },
+  {
+    "revision": "1907724f53ba819377eaee81dc7a5563",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/slot3.png"
+  },
+  {
+    "revision": "78e77086e2faac3656254df0c1a99055",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/slot4.png"
+  },
+  {
+    "revision": "157e880620a46d770d98ee271bbf9c1a",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/slot5.png"
+  },
+  {
+    "revision": "30913fef2ca75685ad3ba1dfb1452ab5",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaassets/images/slot6.png"
   },
   {
     "revision": "f1d999905000226c4165f9c360232913",
@@ -84,15 +132,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betafavicon.ico"
   },
   {
-    "revision": "d7e11cc4a19b06665691",
-    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betagen.main.e54d44260479408fb80b.bundle.js"
+    "revision": "aeb63b27d767df43bfdc",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betagen.main.5f3275e020bad2285aac.bundle.js"
   },
   {
-    "revision": "11169d7ee1d44bc7faa3",
-    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betagen.vendors.b4a63638355cb693c278.bundle.js"
+    "revision": "79244101a84faa626992",
+    "url": "/educational-game-project-joel-hafycz-ryan-melillo/betagen.vendors.b45a024788a7c2f75858.bundle.js"
   },
   {
-    "revision": "13119aebcb44c25bd61d9fba24d67e3b",
+    "revision": "4240e3e1daf66fabb15d9a60685f793c",
     "url": "/educational-game-project-joel-hafycz-ryan-melillo/betaindex.html"
   },
   {
--- beta/sw.js
+++ beta/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-game-project-joel-hafycz-ryan-melillo/betaprecache-manifest.471cda8f6190262014bf396df8555dd2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-joel-hafycz-ryan-melillo/betaprecache-manifest.9b69a7c50d6db1452a7cb415afe5386a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
