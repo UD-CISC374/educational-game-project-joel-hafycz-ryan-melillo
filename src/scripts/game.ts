f51abc2617b6eff68f506105799849ba1a12c886
jhaf <jhaf@udel.edu>
2020-05-17 23:27:02 -0400

    Im dumb i forgot to add them in game.ts

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -1,9 +1,12 @@
 import 'phaser';
+import Level0 from './scenes/Level0';
 import Level1 from './scenes/Level1';
 import Level2 from './scenes/Level2';
 import Level3 from './scenes/Level3';
+import Level4 from './scenes/Level4';
 import PreloadScene from './scenes/preloadScene';
 import GameConfig = Phaser.Types.Core.GameConfig;
+
 
 const DEFAULT_WIDTH = 940;
 const DEFAULT_HEIGHT = 700;
@@ -18,7 +21,7 @@ const config: GameConfig = {
         width: DEFAULT_WIDTH, 
         height: DEFAULT_HEIGHT
     },
-    scene: [PreloadScene, Level1, Level2, Level3],
+    scene: [PreloadScene, Level0, Level1, Level2, Level3, Level4],
     physics: {
         default: 'arcade',
         arcade: {
